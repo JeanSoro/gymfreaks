a3d93e12169d54d853ca1d43891b300178481ef0
Jean <jean.soro007@gmail.com>
2019-10-28 19:20:18 -0400

    Implement video pause/start function

--- js/app.js
+++ js/app.js
@@ -4,35 +4,60 @@
 class UI {
 
   hidePreloader() {
-    return document.querySelector('.preloader').style.display = 'none';
+    document.querySelector('.preloader').style.display = 'none';
   }
 
   showNav() {
-    return document.querySelector('.nav').classList.toggle('nav--show');
+    document.querySelector('.nav').classList.toggle('nav--show');
   }
+
+  videoControls() {
+    let btn = document.querySelector('.video__switch-btn');
+
+    if (!btn.classList.contains('btnSlide')) {
+      btn.classList.add('btnSlide')
+      document.querySelector('.video__item').pause();
+
+    } else {
+      btn.classList.remove('btnSlide')
+      document.querySelector('.video__item').play();
+    }
+  }
+
+
 }
 
 
 function eventListeners() {
 
-  const ui = new UI()
+  const ui = new UI();
 
   //window event listener
-  window.addEventListener('load', function () {
+  window.addEventListener('load', () => {
     ui.hidePreloader();
   })
 
 
   // nav button event listener
-  document.querySelector('.navBtn').addEventListener('click', function () {
+  document.querySelector('.navBtn').addEventListener('click', () => {
     ui.showNav();
   });
 
+  document.querySelector('.video__switch').addEventListener('click', () => {
+    ui.videoControls();
+  })
+
 }
+
 eventListeners();
 
 
 
+
+// contructor es5
+// function UI(){
+
+//}
 
 
 // UI.prototype.hidePreloader = function () {
